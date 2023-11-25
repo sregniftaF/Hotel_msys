7cc9270cd6c700547d84bd618034ac0d49937e28
Haku1234 <satoshing115@gmail.com>
2023-11-25 21:03:57 +0800

    Updated hotelinfo.html, and the app.py for hotelBooking

--- webpage/travelista-master/travelista-master/website/app.py
+++ webpage/travelista-master/travelista-master/website/app.py
@@ -441,16 +441,24 @@ def hotelbooking():
         r_d = session.get('room_data')
         t_room = session.get('rooms')
 
-        for room_num in range(1, int(t_room) + 1):
-            x = r_d.get(f'room_{room_num}')
-            print(x.get('adults'), x.get('child'), x.get('childage'))
+        # for room_num in range(1, int(session['rooms']) + 1):
+        #     x = r_d.get(f'room_{room_num}')
+        #     print(x.get('adults'), x.get('child'), x.get('childage'))
 
-        print(rtype, t_price, n_pax, check_in, check_out, date_duration, r_d)
-        cursor.execute(
-            'INSERT into hotelDatabase.booking VALUES( NULL, %s, %s, %s, %s, %s, %s, %s, %s)',
-            (session['id'], session['p_id'], rtype, n_pax, t_price, check_in, check_out, date_duration)
-        )
-        mysql.connection.commit()
+        if check_in is None:
+            flash('Please enter Detail in the Homepage!')
+        elif check_out is None:
+            flash('Please enter Detail in the Homepage!')
+        elif date_duration is None:
+            flash('Please enter Detail in the Homepage!')
+        else:
+            print(rtype, t_price, n_pax, check_in, check_out, date_duration, r_d)
+            cursor.execute(
+                'INSERT into hotelDatabase.booking VALUES( NULL, %s, %s, %s, %s, %s, %s, %s, %s)',
+                (session['id'], session['p_id'], rtype, n_pax, t_price, check_in, check_out, date_duration)
+            )
+            mysql.connection.commit()
+
     return render_template('hotelBooking.html')
 
 if __name__ == '__main__':
